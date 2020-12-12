268563a435850a7c923f65853909cd40e25a8ba8
pratyu <pratyusha.raghupatruni@gmail.com>
2020-12-11 18:49:50 -0800

    fixed an issue with node_modules

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
 
@@ -10,7 +9,7 @@ const app = express();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
