95fc8b61e9e3db1d1db8dfaa10e883c289c2ab07
pratyu <pratyusha.raghupatruni@gmail.com>
2020-12-11 19:21:29 -0800

    added routes in server js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 require("dotenv").config();
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -24,7 +25,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-book-sea
   useFindAndModify: false
 });
 
-
+app.use(routes);
 // Send every other request to the React app
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
