c18f4124c75eb1e9e6f0aa21b688d1362535f77f
pratyu <pratyusha.raghupatruni@gmail.com>
2020-12-13 01:01:59 -0800

    changed the mongodb connection name to google-books-search

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",{
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books-search",{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
